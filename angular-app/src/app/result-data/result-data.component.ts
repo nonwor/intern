4f19fe32cbabbd8413c8d2f432832707703836fd
nonwor <nonwor@gmail.com>
2023-05-14 00:40:29 -0700

    time

--- angular-app/src/app/result-data/result-data.component.ts
+++ angular-app/src/app/result-data/result-data.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, SimpleChanges} from '@angular/core';
-import { Store } from '../store';
+import { Store, TimingErr, DataErr } from '../store';
 import { HttpClient, } from '@angular/common/http';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-result-data',
@@ -15,6 +16,9 @@ export class ResultDataComponent {
   apiUrl:string = " ";
   constructor(private http: HttpClient) { }
 
+  dataError: DataErr[] = [];
+  timeingError: TimingErr[] = [];
+  
   result:any;
   
   ngOnChanges(changes: SimpleChanges) {
@@ -31,17 +35,37 @@ export class ResultDataComponent {
   execute() {
     // this.apiUrl = 'https://swapi.dev/api/people/' +this.searchTerm;
     this.apiUrl ='http://localhost:8080/item/'+this.placeHolder;
-    return this.http.get(this.apiUrl);
+    return this.http.get<Store[]>(this.apiUrl);
   }
 
   getMassiveData(){
     console.log(this.placeHolder);
-
-    this.execute().subscribe((data:any)=>{
+    this.execute().subscribe((data:Store[])=>{
       console.log(data);
       this.result = data;
+      for(const i of this.result){
+        // console.log(i);
+        for(const j of i){
+          // console.log(j);
+          if(Object.keys(j).length == 7){
+            // j.now = new Date(j.now);
+            j.now = moment.utc(j.now);
+            this.dataError.push(j);
+          }else{
+            j.now = moment.utc(j.now);
+            j.paulPulledDateUTC = moment.utc(j.paulPulledDateUTC);
+            j.scheduleExpectedTimeUTC = moment.utc(j.scheduleExpectedTimeUTC);
+            this.timeingError.push(j);
+          }
+        }
+      }
+      //Check for all data?
+      console.log(this.timeingError.length)
+      console.log(this.dataError.length)
+      console.log(this.dataError[0])
+      console.log(this.timeingError[0])
     })
+
   }
   
-
 }
--- angular-app/src/app/store.ts
+++ angular-app/src/app/store.ts
@@ -5,3 +5,26 @@ export interface Store{
     Size: number;
     StorageClass: string;
 }
+
+export interface DataErr{
+    epname: string;
+    cname: string;
+    errType: "timing"|"rowcount";
+    errmsg: string;
+    cwurl: string;
+    schedguid: string;
+    now: Date;
+}
+
+export interface TimingErr{
+    paulPulledDateUTC: Date;
+    scheduleExpectedTimeUTC: Date;
+    epname: string;
+    cname: string;
+    errType: "timing"|"rowcount";
+    errmsg: string;
+    cwurl: string;
+    schedguid: string;
+    awsCronExpression: string;
+    now: Date;
+}
